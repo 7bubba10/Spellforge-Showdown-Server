9697a03b0d1bb4da711d424bd9b350e1407f0ebb
7bubba10 <aidan.decina1@marist.edu>
2025-11-10 19:11:27 -0500

    add /list endpoint to return all active lobbies

--- routes/lobby.js
+++ routes/lobby.js
@@ -13,8 +13,30 @@ router.post("/join", (req, res) => {
         console.log(`[Lobby] Failed to join: Missing Code`);
         return res.status(400).json({error: "Room Code Required"});
     }
-    console.log(`[Lobby] Joined Room: ${roomCode} `)
+    console.log(`[Lobby] Joined Room: ${roomCode} `);
     res.json({status: "Joined", roomCode});
+});
+
+router.get("/list", (req,res) =>{
+    try
+    {
+        // Mock rooms for testing well switch later with database intergration 
+        const mockLobbies = [
+            { roomCode: "TEST123", players: 1 },
+            { roomCode: "XYZ789", players: 2 },
+        ];
+
+        console.log("[Lobby] Sent Lobby List", mockLobbies);
+        res.json({status: "ok", lobbies: mockLobbies});
+    }
+    catch(err)
+    {
+        if(!mockLobbies){
+            console.log("[Lobby] Error sending list:", err);
+            res.status(500).json({error: "Failed to fetch lobby list"});
+        }
+
+    }
 });
 
 module.exports = router;
